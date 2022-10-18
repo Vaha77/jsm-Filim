a153d749451eff95ea7028a19e18edfaab4548bf
RAKHIMOV VOKHIDJON <86535443+Vaha77@users.noreply.github.com>
2022-10-18 02:53:31 -0700

    vaha

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import { AppBar, IconButton, Toolbar, useMediaQuery } from '@mui/material';
+import { Menu, Brightness7 } from '@mui/icons-material';
+// import { AppBar,IconButton,Toolbar,Drawer,Button,Avatar } from '@mui/material';
+// import { Menu } from '@mui/icons-material';
+import useStyles from './styles';
 
 const NavBar = () => {
-  console.log('NavBar');
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px)');
 
   return (
-    <div>
-      NavBar
-    </div>
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{ outline: 'none' }}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={{}}>
+            <Brightness7 />
+          </IconButton>
+        </Toolbar>
+
+      </AppBar>
+    </>
   );
 };
 
