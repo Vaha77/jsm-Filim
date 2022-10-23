a20edf9f0d5a279f956a4328103b176b5656b585
RAKHIMOV VOKHIDJON <86535443+Vaha77@users.noreply.github.com>
2022-10-23 06:23:25 -0700

    vaha

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import { AppBar, IconButton, Toolbar, useMediaQuery } from '@mui/material';
-import { Menu, Brightness7 } from '@mui/icons-material';
-// import { AppBar,IconButton,Toolbar,Drawer,Button,Avatar } from '@mui/material';
-// import { Menu } from '@mui/icons-material';
+import React, { useState } from 'react';
+
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
+import { Menu, Brightness7, Brightness4, AccountCircle } from '@mui/icons-material';
+import { useTheme } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
 import useStyles from './styles';
 
 const NavBar = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
   const classes = useStyles();
   const isMobile = useMediaQuery('(max-width:600px)');
-
+  const theme = useTheme();
+  const isAuthentcated = true;
   return (
     <>
       <AppBar position="fixed">
@@ -24,12 +27,50 @@ const NavBar = () => {
             </IconButton>
           )}
           <IconButton color="inherit" sx={{ ml: 1 }} onClick={{}}>
-            <Brightness7 />
+            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 /> }
           </IconButton>
+          {!isMobile && 'search...'}
+          <div>
+            {!isAuthentcated ? (
+              <Button onClick={() => {}} color="inherit">
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to="/profile/:id"
+                onClick={() => {}}
+              >
+                {!isMobile && <>My Movies &nbsp; </> }
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="profile"
+                  src="https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg"
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && 'Search...'}
         </Toolbar>
-        </Toolbar>
-
       </AppBar>
+      <div>
+        <nav className="classes.drawer">
+          {isMobile ? (
+            <Drawer
+              variant="temporary"
+              anchor="right"
+              open={mobileOpen}
+              onClose={() => {}}
+              classes={{ paper: classes.drawerPaper }}
+              ModalProps={{ keepMounted: true }}
+            />
+          ) : (
+            <Drawer />
+          )}
+
+        </nav>
+      </div>
     </>
   );
 };
