48cbd161dd4a01b7a2eef4d08f8a02037993a605
RAKHIMOV VOKHIDJON <86535443+Vaha77@users.noreply.github.com>
2022-11-04 22:08:10 -0700

    vaha

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,12 +1,17 @@
 import { Grid } from '@mui/material';
 import React from 'react';
+import Movie from '../Movie/Movie';
 
 import useStyles from './styles';
 
 const MovieList = ({ movies }) => {
   const classes = useStyles();
   return (
-    <Grid container className={classes.moviesContainer}>MovieList</Grid>
+    <Grid container className={classes.moviesContainer}>
+      {movies.results.map((movie, i) => (
+        <Movie key={i} movie={movie} i={i} />
+      ))}
+    </Grid>
   );
 };
 
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,12 +1,32 @@
+import { Box, CircularProgress, Typography } from '@mui/material';
 import React from 'react';
-import MovieList from '..';
+import MovieList from '../MovieList/MovieList';
 import { useGetMoviesQuery } from '../Services/TMDB';
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
+  const { data, error, isFetching } = useGetMoviesQuery();
 
-  console.log(data, 'vaha');
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
 
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'An error has occured.';
   return (
     <div>
       <MovieList movies={data} />
