adbf0ea76b1417a8f2545e8d54fb1954ca29d2ed
RAKHIMOV VOKHIDJON <86535443+Vaha77@users.noreply.github.com>
2022-11-08 21:09:19 -0800

    vaha

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,5 +1,6 @@
-import { Grid, Typography } from '@mui/material';
+import { Typography, Grid, Grow } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useStyles from './styles';
 
 const Movie = ({ movie, i }) => {
@@ -7,9 +8,13 @@ const Movie = ({ movie, i }) => {
   console.log(movie, i);
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
-      <Typography className={classes.title} variant="h5">{movie.title}
-
-      </Typography>
+      <Grow in key={i} timeout={(i + 1) * 250}>
+        <Link className={classes.links} to={`/movie/${movie.id}`}>
+          {movie.poster_path ? <img alt={movie.title} className={classes.image} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+            : <img alt={movie.title} className={classes.image} src="https://www.fillmurray.com/200/300" />}
+        </Link>
+      </Grow>
+      <Typography className={classes.title} variant="h5">{movie.title}</Typography>
     </Grid>
   );
 };
