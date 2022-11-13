0235302afa0f2c4109189c6e4a35f6abe60b625a
RAKHIMOV VOKHIDJON <86535443+Vaha77@users.noreply.github.com>
2022-11-13 03:14:33 -0800

    avaha

---
+++ src/Fratures/currentGenreOrCategory.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const genreOrCategory = createSlice({
+    name: 'genreOrCategory',
+    initialState: {
+        genreOrCategoryName:'',
+        page: 1,
+        searchQuery: '',
+
+    },
+    reducers:{
+        selectGenreOrCategory:()
+    }
+})
