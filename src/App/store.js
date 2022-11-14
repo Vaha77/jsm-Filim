a2086446efc0c2508d4c62e7e032bff226da48c0
RAKHIMOV VOKHIDJON <86535443+Vaha77@users.noreply.github.com>
2022-11-14 04:19:33 -0800

    vaha

--- src/App/store.js
+++ src/App/store.js
@@ -5,4 +5,11 @@ export default configureStore({
   reducer: {
     [tmdbApi.reducerPath]: tmdbApi.reducer,
   },
+});import { configureStore } from '@reduxjs/toolkit';
+import { tmdbApi } from '../Services/TMDB';
+
+export default configureStore({
+  reducer: {
+    [tmdbApi.reducerPath]: tmdbApi.reducer,
+  },
 });
