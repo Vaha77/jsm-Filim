fda99a2c4b70056eac57344a564a1655dba7d944
RAKHIMOV VOKHIDJON <86535443+Vaha77@users.noreply.github.com>
2022-11-21 05:14:02 -0800

    vaha

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -33,14 +33,6 @@ const Sidebar = ({ setMobileOpen }) => {
         />
 
       </Link>
-      <Link to="/" className={classes.imageLink}>
-        <img
-          className={classes.image}
-          src={theme.palette.mode === 'light' ? redLogo : blueLogo}
-          alt="filimLogo"
-        />
-
-      </Link>
 
       <Divider />
       <List>
